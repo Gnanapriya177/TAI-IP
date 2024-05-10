2499411fec0ca07c9cbe92b9440ccad6733a5f76
Gnanapriya177 <133194111+Gnanapriya177@users.noreply.github.com>
2024-05-10 21:02:51 +0530

    Add files via upload

---
+++ Level-1_Task-2_Countdown_Timer.py
@@ -0,0 +1,11 @@
+from time import sleep
+seconds = int(input("Enter Seconds: "))
+if seconds > 0:
+    while seconds >= 0:
+        mins, secs = divmod(seconds, 60)
+        time = "  {:02d}:{:02d}".format(mins,secs)
+        print(time)
+        sleep(1)
+        seconds -= 1
+else:
+    print("Seconds must be greater than 0")
